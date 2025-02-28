20de33f4053b75ed1dd49bd408ff07ef973463f8
Andy Ryan <john.ryan@nenaner.com>
2025-02-28 12:20:31 -0500

    stage-1

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,8 @@
+import { StyleSheet } from 'react-native';
 import { Text, View } from "react-native";
+
+const HEADER_HEIGHT = 75;
+const FOOTER_HEIGHT = 75;
 
 export default function Index() {
   return (
@@ -9,7 +13,31 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        <View style={style.chatHeader}>
+            <Text>Chat Header</Text>
+        </View>
+        <View style={style.chatBody}>
+            <Text>Chat Body</Text>
+        </View>
+        <View style={style.chatFooter}>
+            <Text>Chat Footer</Text>
+        </View>
+
     </View>
   );
 }
+
+const style = StyleSheet.create({
+    chatHeader: {
+        height: HEADER_HEIGHT,
+        backgroundColor: 'lightblue',
+    },
+    chatBody: {
+        flex: 1,
+        backgroundColor: 'lightgreen',
+    },
+    chatFooter: {
+        height: FOOTER_HEIGHT,
+        backgroundColor: 'lightblue',
+    }
+});

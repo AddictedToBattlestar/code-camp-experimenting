76d1a6c74aec0a1ceb91dbf92216e4a207e892d8
Andy Ryan <john.ryan@nenaner.com>
2025-03-16 13:45:42 -0400

    updates while at the library, 2nd weekend

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -2,7 +2,7 @@ import {ScrollView, StyleSheet, ViewStyle} from "react-native";
 import {InitialMessages} from "@/constants/InitialMessages";
 
 import ChatMessage from "@/app/components/ChatMessage";
-import {Fragment, useCallback, useEffect, useState} from "react";
+import {Fragment, useCallback, useState} from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ChatMessageFromSelf from "@/app/components/ChatMessageFromSelf";
 import {useFocusEffect} from "expo-router";
@@ -11,30 +11,29 @@ type Props = {
     style: ViewStyle;
 };
 
-export default function Body({style}: Props) {
+export default function Body({style}: Props): string | null {
     const [userName, setUserName] = useState<string>('');
 
     const getUserName = async () => {
         try {
-            const value = await AsyncStorage.getItem('userName');
-            if (value !== null) {
-                setUserName(value);
-                console.log(`userName: ${value}`);
-            }
+            return await AsyncStorage.getItem('userName');
         } catch (ignoredError) {
-            // error reading value
+            return null;
         }
     };
 
     useFocusEffect(
         useCallback(() => {
-            // Do something when the screen is focused
-            console.log('Hello, I\'m focused!');
-            getUserName();
+            console.debug('This route is now focused');
+
+            getUserName().then((value) => {
+                if (value !== null) {
+                    setUserName(value);
+                    console.log(`userName: ${value}`);
+                }
+            });
             return () => {
-                // Do something when the screen is unfocused
-                // Useful for cleanup functions
-                console.log('This route is now unfocused.');
+                console.debug('This route is now unfocused.');
             };
         }, [])
     );

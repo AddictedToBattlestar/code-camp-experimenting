5b457e2f2858177279237add288b64cbd6f6686e
Andy Ryan <john.ryan@nenaner.com>
2025-04-01 20:02:33 -0400

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,18 @@ import {Stack} from "expo-router";
 import {StatusBar} from "expo-status-bar";
 
 export default function RootLayout() {
-  return (
-      <>
-          <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="+not-found"/>
-              <Stack.Screen name="take-photo"/>
-          </Stack>
-          <StatusBar style="light"/>
-      </>
+    return (
+        <>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{
+                    headerShown: false,
+                    title: 'Home'
+                }}/>
+                <Stack.Screen name="+not-found"/>
+                <Stack.Screen name="take-photo" options={{title: 'Take Photo'}}/>
+            </Stack>
+            <StatusBar style="light"/>
+        </>
 
-  );
+    );
 }

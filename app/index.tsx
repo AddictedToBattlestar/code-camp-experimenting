d864d249b25341380dd4e2fff0194288204e01c3
Andy Ryan <john.ryan@nenaner.com>
2025-04-02 08:00:41 -0400

    wip

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import {StyleSheet, View} from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 import {Colors} from "@/constants/Colors";
 import {Constants} from "@/constants/Constants";
 
 import Body from '@/app/components/chat/Body';
 import Footer from '@/app/components/chat/Footer';
-import {useCallback, useState} from "react";
+import React, {useCallback, useState} from "react";
 import MessageObject from "@/app/objects/MessageObject";
 import {InitialMessages} from "@/constants/InitialMessages";
 import {InitialUserImages} from "@/constants/InitialUserImages";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {useFocusEffect} from "expo-router";
+import {useFocusEffect, useNavigation, useRouter} from "expo-router";
 import MessageType from "@/app/objects/MessageType";
 import ImageData from "@/app/objects/ImageData";
 import uuid from 'react-native-uuid';
@@ -17,6 +17,8 @@ import uuid from 'react-native-uuid';
 // "Index" is a reserved name to indicate the default route to present in the application
 // This will be providing the main chat screen for this project.
 export default function Index() {
+    const navigation = useNavigation();
+    const router = useRouter();
     const [userName, setUserName] = useState<string>('');
 
     const getUserName = async () => {
@@ -31,6 +33,14 @@ export default function Index() {
         useCallback(() => {
             console.debug('This route is now focused');
 
+            navigation.setOptions({
+                headerRight: () => (
+                    <Pressable onPress={() => router.navigate('/profile')}>
+                        <Text>Profile</Text>
+                    </Pressable>
+                ),
+            });
+
             getUserName().then((value) => {
                 if (value !== null) {
                     setUserName(value);
@@ -39,7 +49,7 @@ export default function Index() {
             return () => {
                 console.debug('This route is now unfocused.');
             };
-        }, [])
+        }, [navigation])
     );
 
     const [messages, setMessages] = useState<MessageObject[]>(InitialMessages);

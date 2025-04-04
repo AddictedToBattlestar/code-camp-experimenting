d60e1a351d593b213efc18db8abdc2ec3c564f19
John (Andy) Ryan <john.ryan@cardinalhealth.com>
2025-04-04 15:25:32 -0400

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout() {
                 <Stack.Screen name="index" options={{
                     title: 'Setup User Account'
                 }}/>
+                <Stack.Screen name="home"/>
                 <Stack.Screen name="+not-found"/>
             </Stack>
             <StatusBar style="light"/>

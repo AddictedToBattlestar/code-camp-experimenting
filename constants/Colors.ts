147043a30c7cbf4b1d482f09c255e245f4c9317b
AddictedToBattlestar <addictedtobsg@gmail.com>
2025-04-06 12:57:46 -0400

    color hinting

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,25 +1,25 @@
 // A good source or this stuff: https://coolors.co/
 const PrimaryColorScheme = [
-    '#004B23', // 0
-    '#006400', // 1
-    '#007200', // 2
-    '#008000', // 3
-    '#38B000', // 4
-    '#70E000', // 5
-    '#9EF01A', // 6
-    '#CCFF33', // 7
+    '#004B23', // 0 #004B23
+    '#006400', // 1 #006400
+    '#007200', // 2 #007200
+    '#008000', // 3 #008000
+    '#38B000', // 4 #38B000
+    '#70E000', // 5 #70E000
+    '#9EF01A', // 6 #9EF01A
+    '#CCFF33', // 7 #CCFF33
 ];
 
 const GreyScaleColorScheme = [
-    '#F8F9FA', // 0
-    '#E9ECEF', // 1
-    '#DEE2E6', // 2
-    '#CED4DA', // 3
-    '#ADB5BD', // 4
-    '#6C757D', // 5
-    '#495057', // 6
-    '#343A40', // 7
-    '#212529', // 8
+    '#F8F9FA', // 0 #F8F9FA
+    '#E9ECEF', // 1 #E9ECEF
+    '#DEE2E6', // 2 #DEE2E6
+    '#CED4DA', // 3 #CED4DA
+    '#ADB5BD', // 4 #ADB5BD
+    '#6C757D', // 5 #6C757D
+    '#495057', // 6 #495057
+    '#343A40', // 7 #343A40
+    '#212529', // 8 #212529
 ]
 
 const Colors = {
@@ -37,37 +37,37 @@ export {PrimaryColorScheme, GreyScaleColorScheme, Colors};
 
 
 const OrangeColorScheme = [
-    '#FF7B00', // 0
-    '#FF8800', // 1
-    '#FF9500', // 2
-    '#FFA200', // 3
-    '#FFAA00', // 4
-    '#FFB700', // 5
-    '#FFC300', // 6
-    '#FFD000', // 7
-    '#FFEA00'  // 8
+    '#FF7B00', // 0 #FF7B00
+    '#FF8800', // 1 #FF8800
+    '#FF9500', // 2 #FF9500
+    '#FFA200', // 3 #FFA200
+    '#FFAA00', // 4 #FFAA00
+    '#FFB700', // 5 #FFB700
+    '#FFC300', // 6 #FFC300
+    '#FFD000', // 7 #FFD000
+    '#FFEA00'  // 8 #FFEA00
 ];
 
 const BlueColorScheme = [
-    '#03045E', // 0
-    '#023E8A', // 1
-    '#0077B6', // 2
-    '#0096C7', // 3
-    '#00B4D8', // 4
-    '#48CAE4', // 5
-    '#90E0EF', // 6
-    '#ADE8F4', // 7
-    '#CAF0F8'  // 8
+    '#03045E', // 0 #03045E
+    '#023E8A', // 1 #023E8A
+    '#0077B6', // 2 #0077B6
+    '#0096C7', // 3 #0096C7
+    '#00B4D8', // 4 #00B4D8
+    '#48CAE4', // 5 #48CAE4
+    '#90E0EF', // 6 #90E0EF
+    '#ADE8F4', // 7 #ADE8F4
+    '#CAF0F8'  // 8 #CAF0F8
 ];
 
 const PinkColorScheme = [
-    '#FF0A54', // 0
-    '#FF477E', // 1
-    '#FF5C8A', // 2
-    '#FF7096', // 3
-    '#FF85A1', // 4
-    '#FF99AC', // 5
-    '#FBB1BD', // 6
-    '#F9BEC7', // 7
-    '#FAE0E4'  // 8
+    '#FF0A54', // 0 #FF0A54
+    '#FF477E', // 1 #FF477E
+    '#FF5C8A', // 2 #FF5C8A
+    '#FF7096', // 3 #FF7096
+    '#FF85A1', // 4 #FF85A1
+    '#FF99AC', // 5 #FF99AC
+    '#FBB1BD', // 6 #FBB1BD
+    '#F9BEC7', // 7 #F9BEC7
+    '#FAE0E4'  // 8 #FAE0E4
 ];
